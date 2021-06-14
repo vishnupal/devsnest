38c1a27670122dbdfe12c1baf29881f1f5bd7675
vishnu <vishnupalgehlot@gmail.com>
2021-06-14 21:13:39 +0530

    complete

--- day4/first.js
+++ day4/first.js
@@ -1,8 +1,8 @@
 function first(arr,b=1){
 return arr.slice(0,b)
 }
-// console.log(first([7, 9, 0, -2]));
-// console.log(first([],3));
-// console.log(first([7, 9, 0, -2],3));
-// console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
 console.log(first([7, 9, 0, -2],-3));
